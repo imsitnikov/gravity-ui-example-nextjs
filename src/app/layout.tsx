7837f157ce97ce616f39be1f531485d8fdabe973
Maksim Sitnikov <msitnikov1@yandex-team.ru>
2023-10-12 01:50:28 +0300

    fix: css in production

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
 import type {Metadata} from 'next';
 import {DEFAULT_BODY_CLASSNAME, Wrapper} from '../components/Wrapper';
+
+import '@gravity-ui/uikit/styles/fonts.css';
+import '@gravity-ui/uikit/styles/styles.css';
 import '../styles/globals.scss';
 
 export const metadata: Metadata = {
